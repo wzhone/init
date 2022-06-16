835debcb18b45fd1b4901c77f2a69b54f43a894b
wzhone <i@wzh.one>
2022-06-16 15:56:54 +0800

    add after

--- after.sh
+++ after.sh
@@ -2,5 +2,6 @@
 dnf remove -y kernel-4.18.0 kernel-core-4.18.0 kernel-modules-4.18.0 kernel-devel-4.18.0 kernel-tools-4.18.0 ker nel-tools-libs-4.18.0 kernel-headers-4.18.0
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" /etc/ssh/sshd_config
+sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" /root/.zshrc
 curl https://raw.githubusercontent.com/wzhone/init/master/p10k.zsh -o /root/.p10k.zsh
+echo "sudo -i" >> /home/wzh/.bashrc
