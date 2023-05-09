aae8262072dfc3d07f6c59b36e7c78ffae147a06
wzhone <i@wzh.one>
2023-05-09 16:40:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,14 +25,14 @@ echo "sudo -i" >> /home/wzh/.bashrc
 
 # ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s /bin/zsh root
+chsh -s /bin/zsh `whoami`
 
 # P10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" /root/.zshrc
+sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" ~/.zshrc
 
-curl https://raw.githubusercontent.com/wzhone/init/master/p10k.zsh -o /root/.p10k.zsh
-curl https://raw.githubusercontent.com/wzhone/init/master/zshrc -o /root/.zshrc
+curl https://raw.githubusercontent.com/wzhone/init/master/p10k.zsh -o ~/.p10k.zsh
+curl https://raw.githubusercontent.com/wzhone/init/master/zshrc -o ~/.zshrc
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
